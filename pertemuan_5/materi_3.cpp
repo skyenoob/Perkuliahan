124f65350db7470e9e8cd0020cfafe3f7e94ea7e
root <root@DESKTOP-K1O2BGC.localdomain>
2021-10-20 17:44:55 +0700

    Perulangan

---
+++ pertemuan_5/materi_3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+      int  i, j, n;
+      cout << "Masukkan jumlah baris:  ";
+      cin >> n;
+      for (i = n; i >= 1; i--)
+      {
+            for (j = 1; j <= i; j++)
+            {
+                  cout << "* ";
+            }
+            cout << "\n";
+      }
+      return 0;
+}
