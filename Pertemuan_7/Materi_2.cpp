c96984f4c0e3dff0955c44cde10df09a369c6b93
root <root@DESKTOP-K1O2BGC.localdomain>
2021-10-25 12:14:41 +0700

    Coding Menggunakan String

---
+++ Pertemuan_7/Materi_2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  string var1 = "Belajar C++ ";
+  string var2 = "Pada Mata Kuliah Bahasa Pemograman";
+ 
+  cout << var1 << var2 << endl;
+ 
+  return 0;
+}
