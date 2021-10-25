d374a0fe075c7ca24c6339d66e7c6bd2856ad9b6
root <root@DESKTOP-K1O2BGC.localdomain>
2021-10-25 12:19:38 +0700

    Coding Menggunakan String Sebagai Array

---
+++ Pertemuan_7/Materi_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  string var1 = "Belajar C++ Pada Mata Kuliah Bahasa Pemograman";
+ 
+  cout << var1[0] << endl;
+  cout << var1[1] << endl;
+  cout << var1[2] << endl;
+  cout << var1[3] << endl;
+  cout << var1[4] << endl;
+  cout << var1[5] << endl;
+  cout << var1[6] << endl;
+ 
+  return 0;
+}
