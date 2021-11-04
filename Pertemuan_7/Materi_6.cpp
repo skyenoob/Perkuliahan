417685334bc380393ef445630c8ab3355bedc03d
root <root@DESKTOP-K1O2BGC.localdomain>
2021-11-04 11:58:03 +0700

    String

---
+++ Pertemuan_7/Materi_6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main(){
+    
+    char nama [50] = "Bahasa Pemograman";    
+    char nim [11] = "123457890";
+    cout<<nim<<endl;
+    cout<<nama<< endl;
+    return 0;
+}
