fff6a3500832d96ddd8d43385b6497d1f2579f73
root <root@DESKTOP-K1O2BGC.localdomain>
2021-11-17 17:24:46 +0700

    Jawaban UTS Nomor 5

---
+++ UTS/soal5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    float a,b,c,d,e,f;
+    cout<<"Masukan Angka Pertama: ";
+    cin>>a;
+    cout<<"Masukan Angka Kedua: ";
+    cin>>b;
+    c = a+b;
+    d = a-b;
+    e = a*b;
+    f = a/b;
+    cout<<"Hasil Penjumlahan = "<<c<<endl;
+    cout<<"Hasil Pengurangan = "<<d<<endl;
+    cout<<"Hasil Perkalian= "<<e<<endl;
+    cout<<"Hasil Pembagian = "<<f<<endl;
+    return 0;
+}
