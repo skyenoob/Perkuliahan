469c7a2d7acb24fe390e2426a1a79bc2bf0f0b8e
root <root@DESKTOP-K1O2BGC.localdomain>
2021-11-17 17:11:18 +0700

    Jawaban UTS Nomor 3

---
+++ UTS/soal3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n, s, i, j;
+    cout << "Masukkan jumlah baris : ";
+    cin >> n;
+    for (i = 1; i <= s; i++){
+        for (s = i; s < n; s++)
+        cout << " ";
+        for (j = 1; j <= i; j++)
+        cout << "*";
+        cout << "\n";
+    }
+    return 0;
+}
