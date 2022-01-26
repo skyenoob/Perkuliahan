e2d3d00d5979b916609e59ac405c284bd52f725e
root <root@DESKTOP-K1O2BGC.localdomain>
2022-01-26 21:27:07 +0700

    Jawaban UAS Nomor 2

---
+++ UAS/Jawaban_Nomor_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+void nama(){
+    cout<<"Nama saya adalah Edward Pieters"<<endl;
+}
+
+void nim(){
+    cout<<"NIM saya adalah 20200801020"<<endl;
+}
+
+int main(){
+    nama();
+    nim();
+}
