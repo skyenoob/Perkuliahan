a91b185facb80d17eecd3c0f311779708eda74ad
root <root@DESKTOP-K1O2BGC.localdomain>
2022-01-26 17:17:36 +0700

    Jawaban UAS Nomor 1

---
+++ UAS/Jawaban_Nomor_1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    string nama = "Edward Pieters";
+    string nim = "20200801020";
+
+    cout<<"Nama saya adalah : "<<nama<<endl;
+    cout<< "Alamat memory Nama : ";
+    cout<< &nama <<endl;
+
+    cout <<"NIM saya adalah : "<<nim<<endl;
+    cout << "Alamat memory NIM : ";
+    cout << &nim <<endl;
+}
